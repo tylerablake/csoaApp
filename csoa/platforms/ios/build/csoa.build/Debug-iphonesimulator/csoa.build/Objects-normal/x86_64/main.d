12e0753d7bb9245a538d5da6bbacfc729e2bab22
Tyler Blake <tyler.blake.louisville@gmail.com>
2017-01-30 14:18:47 -0500

    initial commit

---
+++ csoa/platforms/ios/build/csoa.build/Debug-iphonesimulator/csoa.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/Tyler/Documents/github/csoaApp/csoa/platforms/ios/internal/main.m \
+  /Users/Tyler/Documents/github/csoaApp/csoa/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/Tyler/Documents/github/csoaApp/csoa/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/Tyler/Documents/github/csoaApp/csoa/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/Tyler/Documents/github/csoaApp/csoa/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/Tyler/Documents/github/csoaApp/csoa/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Tyler/Documents/github/csoaApp/csoa/platforms/ios/internal/TNSDebugging.h
